53d308a319e4b9545f40f996b33219b6d7341758
Juan Cruz Leyba <jleyba@itx.com>
2019-01-25 09:29:33 -0300

    basic starter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
 
 class App extends Component {
   render() {
@@ -8,21 +10,43 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Bienvenidos</p>
+          <Query query={HELLO_QUERY}>
+            {props => {
+              console.log(props);
+              const { data, loading, error, refetch } = props;
+              if (loading) {
+                return <div>Loading</div>;
+              }
+
+              if (error) {
+                return <div>An unexpected error occurred</div>;
+              }
+
+              setTimeout(() => {
+                refetch({
+                  name: 'Link',
+                });
+              }, 5000);
+
+              return (
+                <div>
+                  <p>What's your name?</p>
+                  <h3>{data.hello}</h3>
+                </div>
+              );
+            }}
+          </Query>
         </header>
       </div>
     );
   }
 }
+
+const HELLO_QUERY = gql`
+  query HelloQuery($name: String) {
+    hello(name: $name)
+  }
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
+
+const Client = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<Client />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
