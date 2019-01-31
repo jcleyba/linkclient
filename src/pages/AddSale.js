ce02759aff367b378905669cbaff3986ccc67633
Juan Cruz Leyba <jleyba@itx.com>
2019-01-30 21:57:42 -0300

    first approach on product search

--- src/pages/AddSale.js
+++ src/pages/AddSale.js
@@ -1,7 +1,61 @@
 import React from 'react';
 
-const AddSale = props => {
-  return <h1>Vender</h1>;
-};
+import { SEARCH_QUERY } from '../queries/products';
+import { Query } from 'react-apollo';
+import { Input } from 'semantic-ui-react';
 
-export default AddSale;
+export default class AddSale extends React.Component {
+  state = {
+    term: '',
+  };
+
+  renderResults = data => {
+    return (
+      data.search &&
+      data.search.map((item, index) => {
+        return <div key={index}>{item.description}</div>;
+      })
+    );
+  };
+
+  onInputChange = ({ target }) => {
+    this.setState({ term: target.value });
+  };
+  renderQuery = () => {
+    const { term } = this.state;
+
+    if (term.length < 3) {
+      return <div>Debe ingresar al menos 3 caracteres</div>;
+    }
+
+    return (
+      <Query query={SEARCH_QUERY} variables={{ term }}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+
+          return <div>{this.renderResults(data)}</div>;
+        }}
+      </Query>
+    );
+  };
+
+  renderInput = () => {
+    return (
+      <Input
+        value={this.state.term}
+        onChange={this.onInputChange}
+        placeholder="Producto o codigo"
+      />
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        {this.renderInput()}
+        {this.renderQuery()}
+      </div>
+    );
+  }
+}
