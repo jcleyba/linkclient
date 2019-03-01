c3571267fbad7498216c95d387946203117c9990
Juan Cruz Leyba <jleyba@itx.com>
2019-03-01 14:41:19 -0300

    Adding focus on first result of search

--- src/components/cards/ProductsResult.js
+++ src/components/cards/ProductsResult.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Button, Input, Card, Message, Form } from 'semantic-ui-react';
 
-class Result extends React.Component {
-  state = {
-    amount: 1,
+function Result(props) {
+  const [amount, setAmount] = useState(1);
+  let buttonEl = useRef();
+
+  const onAmountChange = ({ target }) => {
+    setAmount(target.value);
   };
 
-  onAmountChange = ({ target }) => {
-    this.setState({ amount: target.value });
-  };
+  useEffect(() => {
+    if (props.focus) {
+      buttonEl.current.focus();
+    }
+  }, []);
 
-  renderButton = (productAdded, disableByStock) => {
+  const renderButton = (productAdded, disableByStock) => {
     return (
       <Button
         color={productAdded ? 'red' : 'blue'}
         type="submit"
-        disabled={this.state.amount < 1 || productAdded || disableByStock}
+        ref={buttonEl}
+        disabled={amount < 1 || productAdded || disableByStock}
       >
         {productAdded ? 'Agregado' : 'Agregar'}
       </Button>
     );
   };
 
-  renderInput = () => {
-    const { amount } = this.state;
-    const { product, onSelect, productAdded } = this.props;
+  const renderInput = () => {
+    const { product, onSelect, productAdded } = props;
 
     if (product.stock < 1) {
       return <Message error>Sin stock</Message>;
@@ -39,31 +44,29 @@ class Result extends React.Component {
           min="1"
           max={product.stock}
           value={amount}
-          onChange={this.onAmountChange}
+          onChange={onAmountChange}
           placeholder="Cantidad"
         >
           <input />
-          {this.renderButton(productAdded, disableByStock)}
+          {renderButton(productAdded, disableByStock)}
         </Input>
       </Form>
     );
   };
 
-  render() {
-    const { product } = this.props;
-    const { description, costPrice, salePrice, stock } = product;
+  const { product } = props;
+  const { description, costPrice, salePrice, stock } = product;
 
-    return (
-      <Card>
-        <Card.Content>
-          <Card.Header>{description}</Card.Header>
-          Costo: <b>${costPrice}</b> - Precio: <b>${salePrice} </b>- Stock:{' '}
-          <b>{stock}</b>
-        </Card.Content>
-        <Card.Content extra>{this.renderInput()}</Card.Content>
-      </Card>
-    );
-  }
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{description}</Card.Header>
+        Costo: <b>${costPrice}</b> - Precio: <b>${salePrice} </b>- Stock:{' '}
+        <b>{stock}</b>
+      </Card.Content>
+      <Card.Content extra>{renderInput()}</Card.Content>
+    </Card>
+  );
 }
 
 export default Result;
